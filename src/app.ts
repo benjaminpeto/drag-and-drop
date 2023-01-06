1a5869957c13831e0882f60281ed0e52d348e49a
benjaminpeto <benjaminpeto23@gmail.com>
2023-01-06 17:47:30 +0000

    filtering projects with enum, fix bug which rendered whole list on every submit

--- src/app.ts
+++ src/app.ts
@@ -111,7 +111,14 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(prj => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
+        } else {
+          return prj.status === ProjectStatus.Finished;
+        }
+      })
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -121,6 +128,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById( `${this.type}-projects-list`)! as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
